912f6efac724d77062c3615e96f26f374d87bdb2
Joseph Sim <joseph_sim19@hotmial.com>
2017-10-26 08:55:21 +0800

    shopping cart using user session

--- deallo/process_showcart.php
+++ deallo/process_showcart.php
@@ -6,7 +6,8 @@ $query = $db_handle->getConn()->prepare("SELECT
 b.id, b.product_id, b.quantity, b.date_added, 
 prod.name, prod.price, prod.shipping_fee, prod.seller_id, prod.img
 FROM basket b
-LEFT JOIN products prod on b.product_id = prod.id");
+LEFT JOIN products prod on b.product_id = prod.id
+WHERE b.buyer_username = '".$login_user."'");
 
 $query->execute();
 $results = $query->fetchAll();
