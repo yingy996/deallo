c76ebdc94007a0d4996ab89a6306fb06316425fa
yingy996 <sharonlo996@hotmail.com>
2017-10-28 20:06:48 +0800

    update-order-sql-file
    *Update the new sql file for customer_order table

--- deallo/database imports/customer_order.sql
+++ deallo/database imports/customer_order.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 28, 2017 at 06:54 AM
+-- Generation Time: Oct 28, 2017 at 02:04 PM
 -- Server version: 10.0.17-MariaDB
 -- PHP Version: 5.5.30
 
@@ -27,11 +27,44 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `customer_order` (
+  `id` int(11) NOT NULL,
   `order_id` varchar(6) NOT NULL,
   `product_id` varchar(6) NOT NULL,
   `product_quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `customer_order`
+--
+
+INSERT INTO `customer_order` (`id`, `order_id`, `product_id`, `product_quantity`) VALUES
+(4, 'EST615', 'PDI435', 2),
+(5, 'ESA375', 'EHP544', 2),
+(6, 'ESA375', 'SBS018', 5),
+(7, 'GVF243', 'PDI435', 1),
+(8, 'MRP730', 'SBS018', 1),
+(9, 'HDE147', 'EHP544', 1),
+(10, 'HDE147', 'SBS018', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customer_order`
+--
+ALTER TABLE `customer_order`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customer_order`
+--
+ALTER TABLE `customer_order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
