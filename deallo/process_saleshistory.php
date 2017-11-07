520a22e9001b97cc1e6bf18fcc69ec81c35402c4
yingy996 <sharonlo996@hotmail.com>
2017-11-07 23:05:44 +0800

    fix-display-sales-history
    *Fixed sales history page to display only "Paid", "Processing" and "Delivered" orders.

--- deallo/process_saleshistory.php
+++ deallo/process_saleshistory.php
@@ -7,14 +7,14 @@
 	require_once("dbcontroller.php");
 	
 	$db_handle = new DBController();
-    $query = $db_handle->getConn()->prepare("SELECT order_details.order_id, order_details.customer_id, order_details.order_date, order_details.status, order_details.status_date, order_details.order_price FROM order_details WHERE order_details.seller_id = :username ORDER BY order_details.status_date DESC");
+    $query = $db_handle->getConn()->prepare("SELECT order_details.order_id, order_details.customer_id, order_details.order_date, order_details.status, order_details.status_date, order_details.order_price FROM order_details WHERE order_details.seller_id = :username AND (order_details.status <> 'Canceled' AND order_details.status <> 'Not Paid') ORDER BY order_details.status_date DESC");
 	$query->bindParam(":username", $login_user);
 
     $query->execute();
     $orderResult = $query->fetchAll();
 
     //Query to get monthly sales of user
-    $monthlysalesquery = $db_handle->getConn()->prepare("SELECT YEAR(order_date) as SalesYear, MONTH(order_date) as SalesMonth, SUM(order_price) as TotalSales FROM order_details WHERE seller_id = :username AND order_date > DATE_SUB(now(), INTERVAL 12 MONTH) AND (status <> 'Canceled' AND status <> 'Not Paid' ) GROUP BY YEAR(order_date), MONTH(order_date) ORDER BY YEAR(order_date), MONTH(order_date) DESC");
+    $monthlysalesquery = $db_handle->getConn()->prepare("SELECT YEAR(order_date) as SalesYear, MONTH(order_date) as SalesMonth, SUM(order_price) as TotalSales FROM order_details WHERE seller_id = :username AND order_date > DATE_SUB(now(), INTERVAL 12 MONTH) AND (status <> 'Canceled' AND status <> 'Not Paid') GROUP BY YEAR(order_date), MONTH(order_date) ORDER BY YEAR(order_date), MONTH(order_date) DESC");
     $monthlysalesquery->bindParam(":username", $login_user);
 	
     $monthlysalesquery->execute();
